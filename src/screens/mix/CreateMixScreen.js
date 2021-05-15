748c3e7cc76b72c7f1412b8e193a9f7e31300290
TIAGO-DELL <tiagolbassani@gmail.com>
2021-05-15 13:42:46 -0300

    Minor changes

--- src/screens/mix/CreateMixScreen.js
+++ src/screens/mix/CreateMixScreen.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import {useSelector, useDispatch} from 'react-redux';
+import * as actions from '../../store/actions';
+
 import ScreenWrapper from '../../components/hoc/ScreenWrapper';
+import Input from '../../components/UI/Input';
+import PrimaryButton from '../../components/UI/PrimaryButton';
+
+import Colors from '../../constants/Colors';
+import Sizes from '../../constants/Sizes';
+
+const INPUT_UPDATE = 'UPDATE';
+const CLEAR_FORM = 'CLEAR_FORM';
+
+import formReducer from '../../shared/formReducer';
 
 const CreateMixScreen = props => {
   return (
