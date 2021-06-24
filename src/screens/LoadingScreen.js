1f97dd54f5aa94d2cb07800dea74db9998088423
TIAGO-DELL <tiagolbassani@gmail.com>
2021-06-24 19:26:32 -0300

    Remove unsed import

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 
 import LoadingSpinner from '../components/UI/LoadingSpinner';
 import Colors from '../constants/Colors';
